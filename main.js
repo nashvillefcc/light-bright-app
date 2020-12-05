0f185ee5835bb9ec95d2591b42e2ee813fd3c4ea
beshoyfarag <37483920+beshoyfarag@users.noreply.github.com>
2020-12-04 19:04:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-alert("hello world  from lightbright main.js ");
+
+const addPegs = () => {
+  const numRows = 36;
+  const numColumns = 48;
+  const boardElem = document.getElementById("peg-board");
+  let rowElem = document.createElement("div");
+  rowElem.className = "board-row";
+  let rowInnerHtml = ``;
+
+  // Generate a row with numColumns number of pegs
+  for (var i = 0; i < numColumns; i++) {
+    rowInnerHtml += `<div class="board-peg">&nbsp;</div>`;
+  }
+  rowElem.innerHTML = rowInnerHtml;
+
+  // Add that row to the board numRows number of times
+  for (var i = 0; i < numRows; i++) {
+    boardElem.innerHTML += rowElem.outerHTML;
+  }
+};
+
+addPegs();
